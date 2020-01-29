3254e4200184a11bb64833ca862f3c33239a4bb7
Фёдоров Арсений Николаевич <Arseniy.Fedorov@russianpost.ru>
2020-01-29 12:21:17 +0300

    lesson_1_homework_task_3

--- lesson_35_node_1/homework/task_3/task3.js
+++ lesson_35_node_1/homework/task_3/task3.js
@@ -4,8 +4,12 @@ const fs = require('fs'); // подключение модуля для рабо
 const header = "header.html";
 const body = "body.html";
 const footer = "footer.html";
+
+
 http.createServer((request, response) => {// вызов метода создания http сервера
-    fs.readFile(filename, 'utf8', (err, data1) => {
+    let fullBody = "";
+
+    fs.readFile(header, 'utf8', (err, data) => {
         if (err) {
             console.log('Could not find or open file for reading\n');
             response.statusCode = 404;
@@ -15,23 +19,31 @@ http.createServer((request, response) => {// вызов метода созда�
             // response.writeHead(200, {'Content-Type':'text/html'});
             // response.end(data);
 
-            fs.readFile(body, 'utf8', (err, data2) => {
+            fullBody += data;
+            fs.readFile(body, 'utf8', (err, data) => {
                 if (err) {
                     console.log('Could not find or open file for reading\n');
                     response.statusCode = 404;
                     response.end();
                 } else {
-
+                    fullBody += data;
+                    fs.readFile(footer, 'utf8', (err, data) => {
+                        if (err) {
+                            console.log('Could not find or open file for reading\n');
+                            response.statusCode = 404;
+                            response.end();
+                        } else {
+                            fullBody += data;
+                            response.writeHead(200, {'Content-Type': 'text/html'});
+                            response.end(fullBody);
+                        }
+                    })
                 }
-
             });
         }
-
-
-
-
     });
+
     console.log("Request accepted!");
-}).listen(8080, ()=>{
+}).listen(8080, () => {
     console.log("HTTP server works in 8080 port!\n");
 });
