3769a2be7c5b4e1d6888c2fb0a0bb0a8f851f2f1
Фёдоров Арсений Николаевич <Arseniy.Fedorov@russianpost.ru>
2020-02-03 10:30:35 +0300

    lesson_37_node_3 lab_task_1_part_b

--- lesson_37_node_3/homework/lab/task_1/server.js
+++ lesson_37_node_3/homework/lab/task_1/server.js
@@ -3,27 +3,58 @@ const fs = require('fs'); // подключение модуля для рабо
 
 
 http.createServer((request, response) => {// вызов метода создания http сервера
-    // console.log(console.log(process.env));
-    let arguments = process.argv;
+    let environment = process.env;
 
     let pathname;
-    if (arguments.includes("ru")){
-        pathname = "ru.html";
-    } else {
-        pathname = "en.html";
-    }
-
-    fs.readFile(pathname, 'utf8', (err, data) => {
-        if (err) {
-            console.log('Could not find or open file for reading\n');
-            response.statusCode = 404;
-            response.end();
+    if (environment.LANG) {
+        if (environment.LANG === "ru_RU") {
+            pathname = "ru.html";
         } else {
-            console.log(`The file ${pathname} is read and sent to the client\n`);
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(data);
+            pathname = "en.html";
         }
-    });
+
+        fs.readFile(pathname, 'utf8', (err, data) => {
+            if (err) {
+                console.log('Could not find or open file for reading\n');
+                response.statusCode = 404;
+                response.end();
+            } else {
+                console.log(`The file ${pathname} is read and sent to the client\n`);
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.end(data);
+            }
+        });
+    } else {
+        // Данная переменная действует только на время работы сервера.
+        console.log("Выставялем переменную LANG автоматически");
+
+        let arguments = process.argv;
+
+        if (arguments.includes("ru")) {
+            if (environment.OS.toLowerCase().includes("windows")) {
+                process.env['LANG'] = 'ru_RU';
+                pathname = "ru.html";
+            } else {
+                process.env['LANG'] = 'ru_RU';
+                pathname = "ru.html";
+            }
+        } else {
+            if (environment.OS.toLowerCase().includes("windows")) {
+                process.env['LANG'] = 'en_EN';
+                pathname = "en.html";
+            } else {
+                process.env['LANG'] = 'en_EN';
+                pathname = "en.html";
+            }
+        }
+
+        // После выставления переменной страницу надо перезагрузить.
+        // Незаметно для пользователя обновляем страницу.
+        response.statusCode = 302; // временная переадресация
+        response.setHeader("Location", "/");
+        response.end();
+
+    }
 
 
 }).listen(8080, () => {
