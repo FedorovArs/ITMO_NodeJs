8f36b9e0abeb32df4f3c6f89bd0a15649efba6d5
Arsenii <Fedorov-Ars@yandex.ru>
2020-02-18 18:43:25 +0300

    lesson_43_vue_2_node_9_classwork

---
+++ lesson_43_vue_2_node_9/test_site/sample/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="$store.commit('increment')">{{ $store.state.counter }}</button>
+    <h1>pages/index.vue</h1>
+    <nuxt-link :to="{ name:'index' }">
+      pages/index.vue
+    </nuxt-link>
+    <nuxt-link :to="{ name:'team' }" no-prefetch>
+      pages/team/index.vue
+    </nuxt-link>
+    <nuxt-link :to="{ name:'team-about' }" no-prefetch>
+      pages/team/about.vue
+    </nuxt-link>
+    <nuxt-link :to="{ name:'users-id', params: {id:1, test:2} }" no-prefetch>
+      User id link
+    </nuxt-link>
+  </div>
+</template>
---
+++ lesson_43_vue_2_node_9/test_site/sample/pages/users/_id.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>User {{$route.params.id}}</h1>
+  </div>
+</template>
+
+<script>
+</script>
