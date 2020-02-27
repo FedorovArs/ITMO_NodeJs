db5ffad00004c9cf094761a7cc7b83c0ddd04f37
Arsenii <Fedorov-Ars@yandex.ru>
2020-02-27 21:37:49 +0300

    lesson_47_node_13_promise_classwork

---
+++ lesson_47_node_13_promise/classwork/many_files/with_promise_async_await.js
@@ -0,0 +1,39 @@
+let http = require('http');
+let fs = require('fs');
+//let fsPromises = fs.promises;
+
+let tasks = ['header.html', 'body.html', 'body2.html', 'footer.html'];
+
+http.createServer(function(req, res){
+    readFiles().then((allData)=>{
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(allData);
+    }).catch((err)=>{
+        console.log(err);
+        res.statusCode = 500;
+        res.end();
+    });
+}).listen(8080);
+
+async function readFiles(){
+    let allData = '';
+
+    try {
+
+        for(let i = 0; i < tasks.length; i++){
+            allData += await promiseReadFile(tasks[i]);
+        }
+
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+
+    return allData;
+}
+
+function promiseReadFile(filename){
+    return fsPromises.readFile(filename, { encoding: 'utf8', flag: 'r' })
+}
